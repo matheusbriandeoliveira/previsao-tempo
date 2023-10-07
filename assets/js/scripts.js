de743e265cbea7cd5db53dcb29ed2b6d5460592f
Matheus Brian <matheusbriandeoliveira@gmail.com>
2023-10-07 19:39:17 -0300

    js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,31 @@
+let chave = "d1993d533ae87d459e6683f91bc5eaf4"
 
-const key = 'd1993d533ae87d459e6683f91bc5eaf4'
 
-function cliqueiNoBotao () {
-  const input = document.querySelector('.input-cidade').value
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
 
-  console.log(input)
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
 }
